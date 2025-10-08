b0bc0ceea4ec5f565bed0903c85e8c93ce6220ca
satyamsiuu <rawatsatyam058@gmail.com>
2025-10-08 23:57:34 +0530

    Done with file handling

---
+++ src/app/fileHandling/ReadEnv.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <io.hpp>
+#include <sstream>
+
+class ReadEnv
+{
+    public:
+        std::string getenv()
+        {
+            std::string env_path = ".env";
+            IO io(env_path);
+            std::fstream f_stream = io.getFileStream();
+            std::stringstream buffer;
+            buffer << f_stream.rdbuf();
+            std::string content = buffer.str();
+            return content;
+        }
+
+};
